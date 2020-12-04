33f3a72431bd681b278c8159c9e4221c55696234
OnniL <onni.lukkarila@gmail.com>
2020-12-04 14:11:23 +0200

    Access allow control origin päälle

--- app.js
+++ app.js
@@ -47,6 +47,31 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});*/
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 // parametrien kirjoitustapa selaimessa : http://localhost:3000/api/players?group=Sikailijat
 app.get("/api/players", function (req, res) {
   console.log("Get players from a certain group");
